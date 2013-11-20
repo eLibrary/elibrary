3035f01d4c048b17ae3750b99592f3a58c8527b8
promankevych <promankevych@romexsoft.com>
2013-11-21 01:17:18 +0200

    drop db-tools-filler from main project

--- src/main/java/com/elib/util/Constants.java
+++ src/main/java/com/elib/util/Constants.java
@@ -8,8 +8,6 @@ package com.elib.util;
 
 public final class Constants {
   
-  public final static String SCAN_RESULT_FILE_NAME = "ScanResult.txt";
-  public final static String FILTER_RESULT_FILE_NAME = "FilterResult.txt";
   public static final String ENGLISH_LANGUAGE = "en";
   public static final String RUSSIAN_LANGUAGE = "ru";
   public static final Integer DEFAULT_DPI_DJVU = 300;
