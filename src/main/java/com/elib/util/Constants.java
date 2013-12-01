5820954091eaaad63939f5390aa56ec41e017a37
promankevych <promankevych@romexsoft.com>
2013-12-02 01:53:53 +0200

    add constants

--- src/main/java/com/elib/util/Constants.java
+++ src/main/java/com/elib/util/Constants.java
@@ -8,9 +8,20 @@ package com.elib.util;
 
 public final class Constants {
   
+  public final static String ELIBRARY_HOST = "http://elibrary.com/";
+  public final static String SCAN_RESULT_FILE_NAME = "ScanResult.txt";
+  public final static String FILTER_RESULT_FILE_NAME = "FilterResult.txt";
   public static final String ENGLISH_LANGUAGE = "en";
   public static final String RUSSIAN_LANGUAGE = "ru";
   public static final Integer DEFAULT_DPI_DJVU = 300;
   public static final Integer DEFAULT_DPI_PDF = 600;
+  public static final String USER_FIRSTNAME_KEY = "firstName";
+  public static final String USER_LASTNAME_KEY = "lastName";
+  public static final String USER_EMAIL_KEY = "email";
+  public static final String USER_PASSWORD_KEY = "password";
+  public static final String USER_DATE_OF_BIRTHD_KEY = "DOB";
+  public static final String USER_ROLE_ADMIN = "admin";
+  public static final String USER_ROLE_GUEST = "guest";
+  public static final String USER_ROLE_REGISTERED = "user";
   
 }
