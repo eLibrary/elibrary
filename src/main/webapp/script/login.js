8cdca7d0db6db517412681bd1ac5344aa2e7c20f
mfedyna <mfedyna@Dawn.RomexSoft.local>
2013-12-02 21:04:58 +0200

    change upload

--- src/main/webapp/script/login.js
+++ src/main/webapp/script/login.js
@@ -16,7 +16,66 @@ function registr(){
 	document.getElementById('login-form').style.visibility = "hidden";
 }
 
+function submitLogin(){
+	var email = document.getElementById('email-input').value;
+	var password = document.getElementById('pass-input').value;
+		$.ajax({
+	        url : '',
+	        type : "POST",
+	        data : {
+	                email : email,
+	                password : password,  
+	        },
+	        success : function(text) {
+	        }
+		});
+}
+
+function search(){
+	var bookSearch = document.getElementById('searchValue').value;
+	var table = '<tr><td>Author</td>td>Title</td>td>edition</td>td>Publisher</td>td>Year</td>td>Extention</td></tr>';
+		$.ajax({
+	        url : '',
+	        type : "POST",
+	        data : bookSearch,
+	        success : function(text) {
+	        	for(var i = 0; i < text.length; i++){
+	        		table += 
+	        			'<tr>'+
+		        			'<td>'+text[i].author+'</td>'+
+		        			'td>'+text[i].title+'</td>'+
+		        			'td>'+text[i].edition+'</td>'+
+		        			'td>'+text[i].publisher+'</td>'+
+		        			'td>'+text[i].year+'</td>'+
+		        			'td>'+text[i].extention+'</td>'+
+	        			'</tr>';
+	        	}
+	        	document.getElementById('search-table').innerHTML = table;
+	        }
+		});
+}
+
+function registration(){
+	var fName = document.getElementById('firstName').value;
+	var lName = document.getElementById('lastName').value;
+	var email = document.getElementById('email-reg').value;
+	var password = document.getElementById('pass').value;
+		$.ajax({
+	        url : '',
+	        type : "POST",
+	        data : {
+	                email : email,
+	                password : password,  
+	        },
+	        success : function(text) {
+	        }
+		});
+}
+
 window.onload = function(){
 	document.getElementById('sign').onclick = login;
 	document.getElementById('registr').onclick = registr;
+	document.getElementById('submitRegistr').onclick = registration;
+	document.getElementById('login').onclick = submitLogin;
+	document.getElementById('search-button').onclick = search;
 }
